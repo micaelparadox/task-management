857e66aea0455e16e4c32f80850d34a4e781a999
Micael Santana <micael.santana@wisereducacao.com>
2022-07-01 10:46:24 -0300

    fix: creating a controller with the tasks and using service.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,19 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
-    }
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
 
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
 }
